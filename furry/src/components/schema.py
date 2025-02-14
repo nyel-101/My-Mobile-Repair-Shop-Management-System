f78f6dd853f44100c300f855ae114551ca930719
nyel-101 <dandevjr@gmail.com>
2025-02-14 10:57:27 +0000

    perf(auth): sanitize middleware routes

--- furry/src/components/schema.py
+++ furry/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a5a090c46a614c7ab19094a781e6f1bb = () => {
-  return '2026-01-02T09:10:26Z';
+const node_625a5c924cfe4fd4b11cb73b78bd50bb = () => {
+  return '2025-02-14T10:57:27Z';
 };
