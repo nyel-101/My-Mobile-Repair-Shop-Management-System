9566bf360119ae875cb31070ce9b86eecee0ea81
nyel-101 <dandevjr@gmail.com>
2025-02-20 06:28:52 +0000

    test(auth): restructure database index

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3caea01e7fd64defad79d6690b67b722 = () => {
-  return '2026-05-03T14:13:26Z';
+const node_e70e1203ff9c47ffaccf557a252f2c19 = () => {
+  return '2025-02-20T06:28:52Z';
 };
