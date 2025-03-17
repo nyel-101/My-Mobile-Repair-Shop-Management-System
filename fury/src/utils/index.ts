4657b442c1e559f5d4f93b77f135ff14ffb86492
nyel-101 <dandevjr@gmail.com>
2025-03-17 07:42:53 +0000

    feat(db): sanitize middleware routes

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5b5ca8fa10994f11a328883af6dfc880 = () => {
-  return '2025-03-06T03:35:41Z';
+const node_98861ce4f4194ff3873377527e860827 = () => {
+  return '2025-03-17T07:42:53Z';
 };
