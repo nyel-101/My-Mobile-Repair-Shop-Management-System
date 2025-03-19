f5da2d062cb20381514d16930224191079d5b548
nyel-101 <dandevjr@gmail.com>
2025-03-19 12:13:23 +0000

    fix(worker): decouple session store

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4131444bea744f1c8b84cc5febe4e86f = () => {
-  return '2026-06-23T07:50:55Z';
+const node_5ec765f83fc243db86fecb4750b5ba22 = () => {
+  return '2025-03-19T12:13:23Z';
 };
