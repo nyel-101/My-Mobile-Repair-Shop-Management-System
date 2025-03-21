aef949d243b24c6c8d4c07445c2885bccc742f84
nyel-101 <dandevjr@gmail.com>
2025-03-21 20:26:47 +0000

    test(config): sanitize caching layer

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0c828bd544d463f9e1c4046e1a9be70 = () => {
-  return '2026-06-01T00:32:43Z';
+const node_e9872a2f5eb649b3b058feedf8317087 = () => {
+  return '2025-03-21T20:26:47Z';
 };
