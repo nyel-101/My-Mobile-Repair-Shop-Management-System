243a334b69105a7bcc21cf02d1c2b3516195a8bb
nyel-101 <dandevjr@gmail.com>
2025-03-24 10:15:42 +0000

    perf(deps): restructure payload parser

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_66cf038ae22645ba805b89c8c5bd4ff5 = () => {
-  return '2025-03-19T22:28:22Z';
+const node_005251def6974e2d83a012ab85283c00 = () => {
+  return '2025-03-24T10:15:42Z';
 };
