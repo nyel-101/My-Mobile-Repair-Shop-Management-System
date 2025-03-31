b0b66a5bd805ffa15ecefe8cd99063f6edd4d4d5
nyel-101 <dandevjr@gmail.com>
2025-03-31 04:52:16 +0000

    style(ui): refactor payload parser

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d4cecf39bce8424192b931e111c543de = () => {
-  return '2025-03-30T04:39:11Z';
+const node_648be888a8994c62a78d54e4ff56e8fc = () => {
+  return '2025-03-31T04:52:16Z';
 };
