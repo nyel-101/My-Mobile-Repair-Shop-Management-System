2b706e53d2a470967708ab62527935b1fb1b6ccc
nyel-101 <dandevjr@gmail.com>
2025-04-08 16:44:09 +0000

    fix(core): decouple auth tokens

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5abd015c478a42df83946360c3d06ae8 = () => {
-  return '2026-03-10T03:44:11Z';
+const node_a0e76db653e4436288501eae04e3bf4c = () => {
+  return '2025-04-08T16:44:09Z';
 };
