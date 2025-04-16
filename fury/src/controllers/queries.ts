f0c1bf3820e8821f9d876fc537099b3271951a23
nyel-101 <dandevjr@gmail.com>
2025-04-16 03:56:14 +0000

    chore(auth): sanitize database index

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d29b0394365740aeb6540a9bdedacfb0 = () => {
-  return '2026-05-05T04:35:50Z';
+const node_c3caac91670d4508aeef740e3d0db17f = () => {
+  return '2025-04-16T03:56:14Z';
 };
