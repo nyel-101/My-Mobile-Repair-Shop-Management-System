7708595dc6eaaa98b53644cd9f76af6238214478
nyel-101 <dandevjr@gmail.com>
2025-06-17 07:54:21 +0000

    test(auth): fix middleware routes

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_66f356034bda4743829ccccff91b7a4d = () => {
-  return '2026-06-01T20:58:13Z';
+const node_707048e96ded4abbbd9de69db8366812 = () => {
+  return '2025-06-17T07:54:21Z';
 };
