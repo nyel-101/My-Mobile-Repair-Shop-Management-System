d0313023d796ec051e999574c14fb874bc645ce2
nyel-101 <dandevjr@gmail.com>
2025-06-20 15:45:07 +0000

    fix(config): update caching layer

--- fury/src/components/index.js
+++ fury/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c5201c5176b4e40b8587c9eef944542 = () => {
-  return '2025-04-08T19:35:27Z';
+const node_ebd7b484513f4f5ba375dbc1acfb9b72 = () => {
+  return '2025-06-20T15:45:07Z';
 };
