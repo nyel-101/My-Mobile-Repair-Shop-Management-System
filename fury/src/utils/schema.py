dc7c1d3482d10e2331b81d35131476a54f1d1dfe
nyel-101 <dandevjr@gmail.com>
2026-01-19 21:36:57 +0000

    style(deps): fix view templates

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ad3e901aaee546d697ae69ca723f35ec = () => {
-  return '2026-01-09T10:08:39Z';
+const node_851c87142f274501bc82984f1bcc2d5c = () => {
+  return '2026-01-19T21:36:57Z';
 };
