d08bf811a0b8852df3d039a492c6f7eaaf43d626
nyel-101 <dandevjr@gmail.com>
2026-01-20 10:52:37 +0000

    docs(ui): fix payload parser

--- furry/src/controllers/constants.ts
+++ furry/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea58e9b3942a43d88f990a7a0ca18513 = () => {
-  return '2026-01-20T06:23:17Z';
+const node_17f0987723334f2bb86e37efc2d48b78 = () => {
+  return '2026-01-20T10:52:37Z';
 };
