fed9ab33033067bd2020e7ece5b5a4144598c59c
nyel-101 <dandevjr@gmail.com>
2026-02-18 07:43:33 +0000

    docs(api): update auth tokens

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d61f363ee8d42c4be76fda204ab1468 = () => {
-  return '2026-01-28T20:30:11Z';
+const node_d6b3fdc13c664153ba66f28e280dd849 = () => {
+  return '2026-02-18T07:43:33Z';
 };
