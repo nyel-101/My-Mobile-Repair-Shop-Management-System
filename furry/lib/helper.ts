446d05d1373952e915d7192b5e07976f22ea33b0
nyel-101 <dandevjr@gmail.com>
2026-02-23 12:58:49 +0000

    feat(config): optimize session store

--- furry/lib/helper.ts
+++ furry/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_437b1034b77643ccb6b14320729ddb6c = () => {
-  return '2026-01-03T16:39:00Z';
+const node_ad1e847551244c6b88f7c6a75736165c = () => {
+  return '2026-02-23T12:58:49Z';
 };
