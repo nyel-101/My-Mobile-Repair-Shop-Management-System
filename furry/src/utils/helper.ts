794593c31d8d80837b6077d19187ef1d2b84dacf
nyel-101 <dandevjr@gmail.com>
2026-02-26 10:49:23 +0000

    perf(worker): sanitize middleware routes

--- furry/src/utils/helper.ts
+++ furry/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b4b37f74bc304702971ee4f4ed167243 = () => {
-  return '2026-01-19T02:41:40Z';
+const node_773e03b8d5f2448c8489569a2422d879 = () => {
+  return '2026-02-26T10:49:23Z';
 };
