29ed972c2998dcdbc2519867b6d035b4f838ceba
nyel-101 <dandevjr@gmail.com>
2026-03-04 20:32:49 +0000

    docs(worker): decouple view templates

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4db155a33dd142fcaac145ed6e0e775d = () => {
-  return '2026-01-12T19:15:03Z';
+const node_49806093671e4264836cbe85808b9851 = () => {
+  return '2026-03-04T20:32:49Z';
 };
