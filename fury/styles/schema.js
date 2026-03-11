ab3ec116895bb35c29c0df03ab34242f993c8d07
nyel-101 <dandevjr@gmail.com>
2026-03-11 10:25:15 +0000

    style(core): implement auth tokens

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2ad856afa794439d8991be31bc5e3dd1 = () => {
-  return '2026-02-03T20:17:48Z';
+const node_8ed621a3e2d64516a1acf1ba28798c46 = () => {
+  return '2026-03-11T10:25:15Z';
 };
