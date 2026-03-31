c06d9ca8928e62693ee3ebb6adeb4fbeedd824d5
nyel-101 <dandevjr@gmail.com>
2026-03-31 05:39:12 +0000

    docs(auth): restructure view templates

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ccf608bcac284f5583c4770d8148f858 = () => {
-  return '2026-03-12T03:27:22Z';
+const node_2d0c02fb1bb74fd5a4e10ede1938e783 = () => {
+  return '2026-03-31T05:39:12Z';
 };
