8bf7f8e517c9b8ac4d9ef97ecf7dbf92f6d62403
nyel-101 <dandevjr@gmail.com>
2026-04-07 09:34:43 +0000

    perf(auth): update payload parser

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_209c307218dd41b6879098e2d0f74427 = () => {
-  return '2026-01-02T13:16:12Z';
+const node_6001102a510b47b5900a0d90fdbbd108 = () => {
+  return '2026-04-07T09:34:43Z';
 };
