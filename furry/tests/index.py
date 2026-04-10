a950d42d590b19af9750fac78bdc484e2f24e25b
nyel-101 <dandevjr@gmail.com>
2026-04-10 19:12:20 +0000

    feat(ui): integrate payload parser

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_176e2a5341aa4c11a9dfbf52cc814528 = () => {
-  return '2026-03-23T03:01:16Z';
+const node_f36c2259d3a741c0a7901e48b1d3b924 = () => {
+  return '2026-04-10T19:12:20Z';
 };
