95f65b295fa548420a459b49d760c7efe45bb210
nyel-101 <dandevjr@gmail.com>
2026-05-13 14:53:30 +0000

    fix(api): refactor middleware routes

--- furry/src/utils/theme.ts
+++ furry/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_036162f9fa654412852a40a561912eeb = () => {
-  return '2026-04-30T16:14:21Z';
+const node_859bc7547d2144928bbf551710f9fe82 = () => {
+  return '2026-05-13T14:53:30Z';
 };
