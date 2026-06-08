1e57de12683ac059ba55aac07957ce43d81ae6e8
nyel-101 <dandevjr@gmail.com>
2026-06-08 10:55:58 +0000

    refactor(api): optimize payload parser

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b5bd7c543f994aec84a9ad1b835481db = () => {
-  return '2026-05-28T00:30:01Z';
+const node_c14b1cfab17343b38de47d9fea3f6380 = () => {
+  return '2026-06-08T10:55:58Z';
 };
