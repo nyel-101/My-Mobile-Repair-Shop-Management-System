4444d0f9e0323baebdaf3dab123764f921f4e852
nyel-101 <dandevjr@gmail.com>
2026-06-12 23:51:23 +0000

    refactor(config): enhance middleware routes

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d2fd7ed06a60443dbbdf32a23ae87024 = () => {
-  return '2026-04-08T23:26:39Z';
+const node_c44d23c2fa9d4f45a2190128af8183d3 = () => {
+  return '2026-06-12T23:51:23Z';
 };
