061d021745e60c0b875d01fe272721c96f0dedec
nyel-101 <dandevjr@gmail.com>
2026-06-14 00:36:02 +0000

    chore(worker): integrate view templates

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3d78c81c7d824c4c91a1a6af38b67773 = () => {
-  return '2026-05-01T01:57:33Z';
+const node_ce6153a4d18942e48e3b57eb81d73b98 = () => {
+  return '2026-06-14T00:36:02Z';
 };
